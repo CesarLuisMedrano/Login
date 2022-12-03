32a1182971b03b7737ae83bfbbf1a45427569580
CesarLuisMedrano <119465468+CesarLuisMedrano@users.noreply.github.com>
2022-12-03 19:19:08 -0400

    Proyecto de prueba
    Tests

---
+++ WebApplication1/TestProject1/APi/Modelos/PersonaTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Modelos;
+
+namespace TestProject1.APi.Modelos
+{
+
+    [TestClass]
+    public class PersonaTest
+    {
+        [TestMethod]
+        public void TestRegistrarPersona()
+        {
+            string nombre = "Fabian";
+            string apellido = "Equise";
+            string resultado = "Registrado";
+            
+            Persona persona = new Persona();
+            string mensaje = persona.RegistrarPersona(nombre, apellido);
+            Assert.AreEqual(resultado, mensaje);
+        }
+
+        [TestMethod]
+        public void TestRegistrarPersonaError()
+        {
+            string nombre = "Fabian";
+            string apellido = "Equise";
+            string resultado = "Error";
+
+            Persona persona = new Persona();
+            string mensaje = persona.RegistrarPersona(nombre, apellido);
+            Assert.AreEqual(resultado, mensaje);
+        }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Modelos/Persona.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication1.Modelos
+{
+    public class Persona
+    {
+        public string nombre;
+        public string apellido;
+
+
+        public string RegistrarPersona(string nombre, string apellido)
+        {
+            this.nombre = nombre;  
+            this.apellido = apellido;
+
+
+            Console.WriteLine(nombre);
+            Console.WriteLine(apellido);
+            return "Registrado";
+        }
+    }
+}
